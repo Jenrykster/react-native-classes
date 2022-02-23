f554058391ffb20d1fc568dc283668f69a49f443
João Henrique <joaoenrique.san@gmail.com>
2022-02-23 16:33:29 -0300

    feat: start project 6

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MealsNavigator from './navigation/MealsNavigator';
+import * as Font from 'expo-font';
+import { useState } from 'react';
 
+const fetchFonts = () => {
+  Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontLoaded, setFontLoaded] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => {
+          console.log(err);
+        }}
+      />
+    );
+  }
+
+  return <MealsNavigator />;
+}
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+
+const renderGridItem = (itemData) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
+
+const CategoriesScreen = (props) => {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    ></FlatList>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+  },
+});
+
+export default CategoriesScreen;
