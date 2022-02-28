eba425752fdc338b87a96072fc99f6d35ea180f2
João Henrique <joaoenrique.san@gmail.com>
2022-02-28 16:41:10 -0300

    feat: start project 8

---
+++ constants/Colors.js
@@ -0,0 +1,4 @@
+export default {
+  primary: '#DD0505',
+  accent: '#000',
+};
---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,29 @@
+import { Platform } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Colors from '../constants/Colors';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import ProductsOverviewScreen from '../screens/shop/ProductOverviewScreen';
+
+const ProductsNavigator = createStackNavigator(
+  {
+    ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+      headerTitleStyle: {
+        fontFamily: 'open-sans-bold',
+      },
+      headerBackTitleStyle: {
+        fontFamily: 'open-sans',
+      },
+    },
+  }
+);
+
+export default createAppContainer(ProductsNavigator);
